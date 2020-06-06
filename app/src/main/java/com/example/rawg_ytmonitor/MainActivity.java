24089aaea2a81e698d8a81ca3684ffa124679f0e
Floriane CARTON <floriane.carton.etu@univ-lille.fr>
2020-06-06 18:25:10 +0200

    feat : add the main activity fragment with his layout

--- app/src/main/java/com/example/rawg_ytmonitor/MainActivity.java
+++ app/src/main/java/com/example/rawg_ytmonitor/MainActivity.java
@@ -1,8 +1,12 @@
-²package com.example.rawg_ytmonitor;
+package com.example.rawg_ytmonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.eightbitlab.bottomnavigationbar.BottomBarItem;
+import com.eightbitlab.bottomnavigationbar.BottomNavigationBar;
+import com.example.rawg_ytmonitor.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationBar bottomNavigationBar = findViewById(R.id.bottom_bar);
+
+        BottomBarItem youtubeItem = new BottomBarItem(R.drawable.ic_yt_logo, R.string.videos);
+        BottomBarItem menuItem = new BottomBarItem(R.drawable.ic_menu_logo, R.string.favorites);
+        BottomBarItem searchItem = new BottomBarItem(R.drawable.ic_search_logo, R.string.search);
+
+        bottomNavigationBar.addTab(youtubeItem);
+        bottomNavigationBar.addTab(menuItem);
+        bottomNavigationBar.addTab(searchItem);
     }
-}²
+}
